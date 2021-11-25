64807d0d83d99f9e93ec602bad58be8f26a27377
anorak6435 <stefan.beckers14@gmail.com>
2021-11-25 11:05:42 +0100

    Add project files.

---
+++ Xamarin_Instructies/Xamarin_Instructies/CodePlusXamlPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Instructies
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CodePlusXamlPage : ContentPage
+    {
+        public CodePlusXamlPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
